9b8f7773b47ea0310a1375bae5e8c547f8f2c519
Scooter Bean <scooterbean@Scooters-MacBook-Pro.local>
2021-05-17 00:36:39 -0400

    minor update

--- server/index.js
+++ server/index.js
@@ -1,44 +1,83 @@
-import https from 'https';
-import express from 'express';
-import path from 'path';
-import fs from 'fs';
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import https from "https";
+import express from "express";
+import xml2js from "xml2js";
+import path from "path";
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const
-  app = express(),
+const app = express(),
+  parseString = xml2js.parseString,
   __dirname = dirname(fileURLToPath(import.meta.url)),
-  hostname = 'localhost',
-  dirToServ = 'client',
+  hostname = "localhost",
+  dirToServ = "client",
   port = 443;
+
 app.use(express.json());
-app.use('/', express.static(path.join(path.resolve(__dirname), '../', dirToServ)));
+app.use(
+  "/",
+  express.static(path.join(path.resolve(__dirname), "../", dirToServ))
+);
 
 const httpsOpts = {
-  cert: fs.readFileSync(path.join(path.resolve(__dirname), '/ssl/xylemtree_dev.crt')),
-  ca: fs.readFileSync(path.join(path.resolve(__dirname), '/ssl/xylemtree_dev.ca-bundle')),
-  key: fs.readFileSync(path.join(path.resolve(__dirname), '/ssl/xylemtree_dev.key'))
-}
+    cert: fs.readFileSync(
+      path.join(path.resolve(__dirname), "/ssl/xylemtree_dev.crt")
+    ),
+    ca: fs.readFileSync(
+      path.join(path.resolve(__dirname), "/ssl/xylemtree_dev.ca-bundle")
+    ),
+    key: fs.readFileSync(
+      path.join(path.resolve(__dirname), "/ssl/xylemtree_dev.key")
+    ),
+  },
+  httpsServer = https.createServer(httpsOpts, app);
 
-const httpsServer = https.createServer(httpsOpts, app);
+app.get("/", (req, res) => {
+  res.send("getting getting");
+  console.log(req.body);
+});
 
-app.get('/', (req, res) => {
-  res.send('testing testing');
-  console.log(req.body);
-})
-app.post('/test', (req, res) => {
-  res.send('besting besting');
-  console.log(req.body);
-})
-app.post('/', (req, res) => {
-  res.send('resting resting');
-  console.log(req.body);
-})
+// app.post("/test", async (req, res) => {
+//   res.send("testing testing");
+//   req &&
+//     parseString(await req.body)
+//       .then(function (result) {
+//         console.dir(result);
+//         console.log("Done");
+//       })
+//       .catch(function (err) {
+//         // Failed
+//       });
+// });
+app.post("/test", (req, res) => {
+  req.on('data', chunk => {
+    parseString(chunk, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+        console.log("Done");
+      }
+    });
+    console.log(`Data chunk available: ${chunk}`)
+  })
+  req.on('end', () => {
+    //end of data
+  })
+  
+  res.send("testing testing");
+});
 
-httpsServer.listen(port, hostname, () => console.log(`Magick in directory ${dirToServ} at https://localhost:${port}`))
+app.post("/", (req, res) => {
+  res.send("posting posting");
+  console.log(req.body);
+});
+
+httpsServer.listen(port, hostname, () =>
+  console.log(`Magick in directory ${dirToServ} at https://localhost:${port}`)
+);
 
 //() => console.log(`Magick in directory ${dirToServ} at https://localhost:${port}`)
-
 
 // const options = {
 //   cert: fs.readFileSync(path.join(path.resolve(__dirname), 'cert.pem')),
@@ -50,7 +89,6 @@ httpsServer.listen(port, hostname, () => console.log(`Magick in directory ${dirT
 //   .listen(port, () => {
 //     console.log(`Magick in directory ${dirToServ} at https://localhost:${port}`)
 //   })
-
 
 // https.createServer(options, (req, res) => {
 //   console.log('fired')
