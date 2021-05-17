ea34202126ccb088f624908640539ed9b4714bc1
Scooter Bean <scooterbean@Scooters-MacBook-Pro.local>
2021-05-17 01:35:01 -0400

    cant tell if mobi is sending me any webhooks

--- server/index.js
+++ server/index.js
@@ -37,20 +37,9 @@ app.get("/", (req, res) => {
   console.log(req.body);
 });
 
-// app.post("/test", async (req, res) => {
-//   res.send("testing testing");
-//   req &&
-//     parseString(await req.body)
-//       .then(function (result) {
-//         console.dir(result);
-//         console.log("Done");
-//       })
-//       .catch(function (err) {
-//         // Failed
-//       });
-// });
 app.post("/test", (req, res) => {
-  req.on('data', chunk => {
+  res.writeHead(200);
+  req.on("data", (chunk) => {
     parseString(chunk, (err, result) => {
       if (err) {
         console.log(err);
@@ -59,13 +48,8 @@ app.post("/test", (req, res) => {
         console.log("Done");
       }
     });
-    console.log(`Data chunk available: ${chunk}`)
-  })
-  req.on('end', () => {
-    //end of data
-  })
-  
-  res.send("testing testing");
+  });
+  res.end("testing testing\n");
 });
 
 app.post("/", (req, res) => {
@@ -76,19 +60,6 @@ app.post("/", (req, res) => {
 httpsServer.listen(port, hostname, () =>
   console.log(`Magick in directory ${dirToServ} at https://localhost:${port}`)
 );
-
-//() => console.log(`Magick in directory ${dirToServ} at https://localhost:${port}`)
-
-// const options = {
-//   cert: fs.readFileSync(path.join(path.resolve(__dirname), 'cert.pem')),
-//   key: fs.readFileSync(path.join(path.resolve(__dirname), 'xylemtree_dev.key'))
-// };
-
-// https
-//   .createServer(options, app)
-//   .listen(port, () => {
-//     console.log(`Magick in directory ${dirToServ} at https://localhost:${port}`)
-//   })
 
 // https.createServer(options, (req, res) => {
 //   console.log('fired')
